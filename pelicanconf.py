bbdccb6a0550c0a67f69bd739f22002cb4c8ede8
Bryce Verdier <bverdier@fastly.com>
2016-03-17 13:42:58 -0700

    updated to get correct feed location.

--- pelicanconf.py
+++ pelicanconf.py
@@ -5,16 +5,18 @@ from __future__ import unicode_literals
 AUTHOR = u'Bryce'
 SITENAME = u'scrollingtext.org'
 SITEURL = 'http://%s' % SITENAME
+#THEME='skeleton'
 
 PATH = 'content'
 
 TIMEZONE = 'America/Los_Angeles'
 
 DEFAULT_LANG = u'en'
+SLUGIFY_SOURCE = 'basename'
 
 # Feed generation is usually not desired when developing
-FEED_ATOM = 'feed/atom.xml'
-FEED_RSS = 'feed/rss.xml'
+FEED_ATOM = 'atom.xml'
+FEED_RSS = 'rss.xml'
 TAG_FEED_ATOM = 'tag/%s/feed/atom.xml'
 TAG_FEED_RSS = 'tag/%s/feed/rss.xml'
 TRANSLATION_FEED_ATOM = None
@@ -28,8 +30,7 @@ AUTHOR_FEED_RSS = None
 #         ('You can modify those links in your config file', '#'),)
 
 # Social widget
-SOCIAL = (('Github', 'https://github.com/btv/'),
-         ('Twitter', 'https://twitter.com/bryceverdier'),)
+SOCIAL = (('Github', 'https://github.com/btv/'),)
 
 DEFAULT_PAGINATION = 10
 
